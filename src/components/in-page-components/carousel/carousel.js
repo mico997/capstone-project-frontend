e7234b1a28d91a64ce7d6bd476cfe3f648f2c9a0
Mico <mico@Micos-MBP.home>
2020-08-03 03:10:29 -0400

    pics added for carousel

--- src/components/in-page-components/carousel/carousel.js
+++ src/components/in-page-components/carousel/carousel.js
@@ -15,35 +15,35 @@ export default class CarouselMain extends Component {
                     <img
                     className="d-block w-100"
                     src="https://m.media-amazon.com/images/I/41q3XfxpmzL._AC_UY218_.jpg"
-                    alt="First slide"
+                    alt="N95 Model 9502"
                     />
                     <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    <h3>N95 Model 9502</h3>
+                    <p>N95 Model 9502 Protective mask</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
                     className="d-block w-100"
-                    src="https://m.media-amazon.com/images/I/41q3XfxpmzL._AC_UY218_.jpg"
-                    alt="Third slide"
+                    src="https://assets.fishersci.com/TFS-Assets/CCG/Dentec-Safety/product-images/1-S-DN5.jpg-650.jpg"
+                    alt="N95 Air Respirator"
                     />
 
                     <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <h3>N95 Air Respirator</h3>
+                    <p>N95 Air Respirator, filter included. Peace with every breath you take!</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
                     className="d-block w-100"
-                    src="https://m.media-amazon.com/images/I/41q3XfxpmzL._AC_UY218_.jpg"
-                    alt="Third slide"
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSckwwCP70zBKq9uRmNHOhkploq5AW4YGB60w&usqp=CAU"
+                    alt="N95 Most effective"
                     />
 
                     <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    <h3>N95 Most effective</h3>
+                    <p>N95 full face mask. Very effective. Minimal air ciriculation garantueed</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 </Carousel>
